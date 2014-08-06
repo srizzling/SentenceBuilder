67bdcf227e5db43005db1bb22cc04a019435c3b0
Sriram Venkatesh <venksriram@gmail.com>
2014-08-07 11:07:31 +1200

    No end keyword:

--- server.rb
+++ server.rb
@@ -1,6 +1,5 @@
 require 'socket'
 path = 'data.txt'
-if File.exists?(file) 
 a = TCPServer.new('', 5950) # '' means to bind to "all interfaces", same as nil or '0.0.0.0'
 loop {
   connection = a.accept
