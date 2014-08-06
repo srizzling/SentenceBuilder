cf9ddb72989837467be0f8a5d5c14a5e2e1fe524
Sriram Venkatesh <venksriram@gmail.com>
2014-08-07 09:49:18 +1200

    Increasing byte size

--- server.rb
+++ server.rb
@@ -2,7 +2,7 @@ require 'socket'
 a = TCPServer.new('', 5950) # '' means to bind to "all interfaces", same as nil or '0.0.0.0'
 loop {
   connection = a.accept
-  text =  connection.recv(1024)
+  text =  connection.recv(200000000)
   puts "received:" + text
   File.open('data.txt', 'w') { |file| file.write(text) }
   connection.write "Sriram has recieved your message"
